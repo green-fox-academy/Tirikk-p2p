26a008a4ee7626a0f8583bd91c8bfd5d7a3d1613
Mate Matiovics <dontirikk@gmail.com>
2017-05-17 13:50:11 +0200

    add methods to get request data as strings

--- src/main/java/com/greenfox/chat/controller/MainController.java
+++ src/main/java/com/greenfox/chat/controller/MainController.java
@@ -4,12 +4,15 @@ import com.greenfox.chat.model.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class MainController {
 
   @RequestMapping("/")
-  public String greeting() {
-    System.err.println(new Log("/", "GET", System.getenv("CHAT_APP_LOGLEVEL"), ""));
+  public String greeting(HttpServletRequest request) {
+    System.err.println(new Log(request.getRequestURI(), request.getMethod(), System.getenv("CHAT_APP_LOGLEVEL"),
+            request.getQueryString()));
     return "index";
   }
 }

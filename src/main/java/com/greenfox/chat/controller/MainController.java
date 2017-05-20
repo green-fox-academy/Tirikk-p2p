b2aafee8eb853246d75e47cbfade00f100ea8ce6
Mate Matiovics <dontirikk@gmail.com>
2017-05-20 21:16:27 +0200

    add refresh link to main page

--- src/main/java/com/greenfox/chat/controller/MainController.java
+++ src/main/java/com/greenfox/chat/controller/MainController.java
@@ -7,6 +7,7 @@ import com.greenfox.chat.model.User;
 import com.greenfox.chat.repository.MessageRepository;
 import com.greenfox.chat.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
